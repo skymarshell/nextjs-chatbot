ed374e89ae35e4fb79d7e221797a99c4c3aa372a
skymarshell <rungradis15946@gmail.com>
2025-05-08 20:05:52 +0700

    แก้ลบตัวแปรที่ไม่ได้ใช้

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Validator from "@/lib/Validator";
 import { useRouter } from "next/navigation";
 import { setLoading } from "@/Components/Loading";
