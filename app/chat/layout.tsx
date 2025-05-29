8fb59c515abd3925b0fb6cb6e5c99711993d6904
skymarshell <rungradis15946@gmail.com>
2025-05-29 12:09:11 +0700

    remove alert

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import Validator from "@/lib/Validator";
 import { useRouter } from "next/navigation";
 import { setLoading } from "@/Components/Loading";
-import DisplayAlert from "@/Components/DisplayAlert";
+// import DisplayAlert from "@/Components/DisplayAlert";
 
 function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   const router = useRouter();
@@ -16,12 +16,12 @@ function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
       const loggedIn = await Validator.IsLoggedIn();
       setIsLoggedIn(loggedIn);
       if (!loggedIn) {
-        DisplayAlert({
-          text: "โปรดเข้าสู่ระบบ",
-          showConfirmButton: false,
-          icon: "warning",
-          timer: 3000,
-        });
+        // DisplayAlert({
+        //   text: "โปรดเข้าสู่ระบบ",
+        //   showConfirmButton: false,
+        //   icon: "warning",
+        //   timer: 3000,
+        // });
         router.push("/login");
       }
       setLoading(false);
