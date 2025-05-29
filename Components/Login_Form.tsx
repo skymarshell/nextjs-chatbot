b68d18719478dca6b763b27426a4596006447a24
skymarshell <rungradis15946@gmail.com>
2025-05-29 12:09:00 +0700

    remove input password

--- Components/Login_Form.tsx
+++ Components/Login_Form.tsx
@@ -74,7 +74,7 @@ function Login_Form() {
               />
             </div>
             <div>
-              <label
+              {/* <label
                 htmlFor="password"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
@@ -87,7 +87,7 @@ function Login_Form() {
                 placeholder="••••••••"
                 className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 ref={password_ref}
-              />
+              /> */}
             </div>
             {/* <div className="flex items-center justify-between">
               <div className="flex items-start">
